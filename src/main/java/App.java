793f9e7cc0fb75c680381500a4304f0f3aca42c2
blackrose9 <sarah.mwendwa@gmail.com>
2020-02-06 22:27:17 +0300

    Hero class, add html skeleton, import gradle dependancies

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,24 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        Map<String, Object> model = new HashMap<String, Object>();
+        get("/", (request, response) -> {
+            return new ModelAndView(model, "squads_gallore.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/view_squad", (request, response) -> {
+            return new ModelAndView(model, "squad_view.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/create_hero", (request, response) -> {
+            return new ModelAndView(model, "create_hero.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
 }
