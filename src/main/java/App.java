74377abd4bc7546f5375edb7a38375d165c82ea6
blackrose9 <sarah.mwendwa@gmail.com>
2020-02-09 07:57:28 +0300

    changed a lot of things trying to fix bugs everywhere, add a squad class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,37 +10,48 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        Map<String, Object> model = new HashMap<String, Object>();
-        ArrayList<Hero> heroes = Hero.getAll();
+
+        post("/create_hero/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Hero> heroes = Hero.getAll();
+
+            String alias = request.queryParams("alias");
+//            get string age and parse it
+            String strAge = request.queryParams("age");
+            Integer age = Integer.parseInt(strAge);
+            String power = request.queryParams("power");
+            String kryptonite = request.queryParams("kryptonite");
+            Hero newHero = new Hero(alias,age,power,kryptonite);
+            model.put("newHero",newHero);
+
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/create_hero", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "create_hero.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/view_all", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Hero> heroes = Hero.getAll();
+            model.put("heroes", heroes);
+            return new ModelAndView(model, "squad_view.hbs");
+        }, new HandlebarsTemplateEngine());
+
 
         get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
             request.session().attribute("username");
             return new ModelAndView(model, "squads_gallore.hbs");
         }, new HandlebarsTemplateEngine());
 
         post("/session", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
             String username = request.queryParams("username");
             request.session().attribute("username", username);
             model.put("username", username);
             return  new ModelAndView(model, "squads_gallore.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        post("/view_squad/new", (request, response) -> {
-            String alias = request.queryParams("alias");
-//            get string age and parse it
-            String strAge = request.queryParams("age");
-            Integer age = Integer.parseInt(strAge);
-
-            String power = request.queryParams("power");
-            String kryptonite = request.queryParams("kryptonite");
-            Hero newHero = new Hero(alias,age,power,kryptonite);
-
-            return new ModelAndView(model, "squad_view.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        get("/view_squad", (request, response) -> {
-            model.put("heroes", heroes);
-            return new ModelAndView(model, "squad_view.hbs");
         }, new HandlebarsTemplateEngine());
 
     }
